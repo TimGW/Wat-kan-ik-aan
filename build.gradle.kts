de39aa30d1d94b7f5e3ca41dd98200966446f89a
Tim Gortworst <tim.gortworst@gmail.com>
2022-08-21 20:21:10 +0200

    fix hilt build issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "7.1.3" apply false
     kotlin("android") version "1.6.10" apply false
     kotlin("kapt") version "1.7.10" apply false
-    id("com.google.dagger.hilt.android") version "2.43.2" apply false
+    id("com.google.dagger.hilt.android") version "2.43.1" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.6.10" apply false
 }
